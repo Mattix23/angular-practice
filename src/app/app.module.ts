cda59704e9e98cc2d5878d551c7702c7e4d9af1d
Mario Torres Jr <torres.mario590@gmail.com>
2023-07-27 13:46:33 -0500

    create material folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DatePickerComponent } from './date-picker/date-picker.component';
 import { RangeSliderComponent } from './range-slider/range-slider.component';
 import { ImageViewComponent } from './image-view/image-view.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -27,14 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSliderModule,
-    MatToolbarModule,
-    MatCardModule,
     HttpClientModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
